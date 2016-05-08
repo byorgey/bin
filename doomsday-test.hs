2c2fa1f041cee8618908e785f1f42c75fe8e9797
Brent Yorgey <byorgey@gmail.com>
2016-05-08 13:54:45 -0500

    tweaks to doomsday tests

--- doomsday-test.hs
+++ doomsday-test.hs
@@ -2,10 +2,14 @@ import           Control.Monad.Random
 import           Data.Char            (toUpper)
 import           Data.List            (isPrefixOf)
 import           Data.Time
-import           System.IO
+
+import           Doomsday
 
 weekday :: Day -> String
 weekday = formatTime defaultTimeLocale "%A"
+
+weekdayNum :: Day -> String
+weekdayNum = formatTime defaultTimeLocale "%w"
 
 randomDay :: Rand StdGen Day
 randomDay = ModifiedJulianDay <$> getRandomR (start, end)
@@ -16,14 +20,13 @@ randomDay = ModifiedJulianDay <$> getRandomR (start, end)
 showDay :: Day -> String
 showDay = formatTime defaultTimeLocale "%B %-d, %Y"
 
-main :: IO ()
-main = do
-  d <- evalRandIO randomDay
-  putStr (showDay d ++ "? ")
-  hFlush stdout
-  start <- getCurrentTime
-  guess <- getLine
-  stop  <- getCurrentTime
-  putStrLn $ if (map toUpper guess `isPrefixOf` map toUpper (weekday d)) then "Right!" else ("Nope, " ++ weekday d ++ ".")
-  putStrLn $ "Time: " ++ show (diffUTCTime stop start)
+doomsdayTest :: IO (Bool, NominalDiffTime)
+doomsdayTest = runTest randomDay showDay
+  (\guess d ->
+     ( (map toUpper guess `isPrefixOf` map toUpper (weekday d)) || (guess == weekdayNum d)
+     , weekday d
+     )
+  )
 
+main :: IO ()
+main = runTests doomsdayTest
