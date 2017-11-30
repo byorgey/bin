f2306ef0dd313c91c2ac0cf5380129c0734f511a
Brent Yorgey <byorgey@gmail.com>
2017-11-30 13:52:07 -0600

    more work on repocheck utility

--- repocheck.hs
+++ repocheck.hs
@@ -1,9 +1,17 @@
-import Control.Monad
-import System.Directory
-import System.FilePath
-import System.Process
+import           Control.Monad
+import           System.Directory
+import           System.FilePath
+import           System.Process
 
-checkRepo :: FilePath -> FilePath -> IO ()
+-- TODO:
+--   1. autodetect git vs darcs
+--   2. colorize output?
+--   3. inline, interactive rectification (i.e. opportunity to record
+--   and push in each repo before moving on to the next)?
+--   hmm... seems like more trouble than it's worth.  Just open a new
+--   terminal.
+
+checkRepo :: FilePath -> FilePath -> IO Bool   -- True <=> clean
 checkRepo home repo = do
   out <- readCreateProcess
            (shell "git status --porcelain 2> /dev/null | tail -n1")
@@ -19,10 +27,14 @@ checkRepo home repo = do
   let ahead = read out :: Int
 
   when (dirty || ahead > 0) $ do
-    putStrLn $ (if dirty then "! " else "  ") ++ (if ahead > 0 then (show ahead) else " ") ++ " " ++ repo
+    putStrLn $ (if dirty then "! " else "  ") ++
+               (if ahead > 0 then (show ahead) else " ") ++ " " ++ repo
+
+  return (not dirty && ahead == 0)
 
 main :: IO ()
 main = do
   home <- getHomeDirectory
   repos <- lines <$> readFile (home </> ".repos")
-  forM_ repos (checkRepo home)
+  clean <- and <$> forM repos (checkRepo home)
+  when clean $ putStrLn "All clean!"
